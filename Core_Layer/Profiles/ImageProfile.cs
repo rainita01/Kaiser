a5743b39bd077892792da234ea947ce19b8f642a
Hamidreza sadani <hamidsadan4@gmail.com>
2026-06-29 14:54:18 +0330

    wwdqwd

--- Core_Layer/Profiles/ImageProfile.cs
+++ Core_Layer/Profiles/ImageProfile.cs
@@ -9,8 +9,7 @@ public class ImageProfile : Profile
     public ImageProfile()
     {
         CreateMap<Image, ImageDto>()
-            .ForMember(e=>e.ImageName,s=>s.MapFrom(e=>e.Name))
-            ;
+            .ForMember(e=>e.ImageName,s=>s.MapFrom(e=>e.Name));
 
     }
 }
